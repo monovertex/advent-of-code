b5f7f1f2b036384b234560d0c5f30effcd4d29b4
Cosmin Stamate <cosmin.stamate@smartbill.ro>
2024-12-10 14:55:48 +0200

    Add 2024-10 solution

---
+++ years/2024/10/solution.ts
@@ -0,0 +1,41 @@
+import { IGraphNode, IMatrix, Point2D, stringToNumberMatrix } from '../../common';
+import '../../prototype-extensions';
+
+function isNeighborValid(_point: Point2D, value: number, _neighborPoint: Point2D, neighborValue: number) {
+    return neighborValue === value + 1;
+}
+
+function computeTrailScore(matrix: IMatrix<number>, startPoint: Point2D): number {
+    const uniqeEndingPoints = new Set<string>();
+    const matcher = (point: Point2D, value: number) => {
+        if (value === 9) uniqeEndingPoints.add(point.toString());
+        return false;
+    };
+    matrix.breadthFirstSearch(startPoint, matcher, isNeighborValid);
+    return uniqeEndingPoints.size;
+}
+
+export function solvePart1(input: string): number {
+    const matrix = stringToNumberMatrix(input);
+    return matrix
+        .filterPoints((point: Point2D, value: number) => value === 0)
+        .map((point) => computeTrailScore(matrix, point))
+        .sum();
+}
+
+function computeTrailRating(matrix: IMatrix<number>, startPoint: Point2D): number {
+    let rating = 0;
+    const walkNode = (node: IGraphNode) => {
+        if (matrix.getValue(node as Point2D) === 9) rating++;
+    };
+    matrix.walk(startPoint, walkNode, isNeighborValid);
+    return rating;
+}
+
+export function solvePart2(input: string): number {
+    const matrix = stringToNumberMatrix(input);
+    return matrix
+        .filterPoints((point: Point2D, value: number) => value === 0)
+        .map((point) => computeTrailRating(matrix, point))
+        .sum();
+}
