78f7653363a170325deb30d66e75c80f721a8bab
Cosmin Stamate <cosmin.stamate@smartbill.ro>
2025-01-20 16:41:51 +0200

    Add 2022-25 solution

---
+++ years/2022/25/solution.ts
@@ -0,0 +1,35 @@
+import '../../prototype-extensions';
+
+const SNAFU_DIGITS = Object.freeze(['=', '-', '0', '1', '2']);
+
+function decimalToSnafu(input: number): string {
+    let digits = [];
+    let remainder = input;
+
+    while (remainder !== 0) {
+        const base5Digit = remainder % 5;
+        if (base5Digit <= 2) digits.push(base5Digit);
+        else {
+            const difference = 5 - base5Digit;
+            remainder += difference;
+            digits.push(-difference);
+        }
+        remainder = Math.floor(remainder / 5);
+    }
+
+    return digits.reverse().map(digit => SNAFU_DIGITS[digit + 2]).join('');
+}
+
+function snafuToDecimal(input: string): number {
+    const digits = input.split('').reverse();
+    return digits.reduce((result, digit, index) => {
+        const base = Math.pow(5, index);
+        if (digit === '-') return result - base;
+        if (digit === '=') return result - base - base;
+        return result + base * Number(digit);
+    }, 0);
+}
+
+export function solvePart1(input: string): string {
+    return decimalToSnafu(input.splitByNewLine().map(snafuToDecimal).sum());
+}
---
+++ years/2022/25/test.ts
@@ -0,0 +1,20 @@
+import chai from 'chai';
+import { describe, it } from 'mocha';
+import { solvePart1 } from './solution';
+import answers from './answers';
+import { readFile } from '../../common';
+
+chai.should();
+
+describe('year 2022, day 25', function () {
+    const inputExample = readFile('./years/2022/25/input-example.txt');
+    const input = readFile('./years/2022/25/input.txt');
+
+    it('part 1 - example', function () {
+        solvePart1(inputExample).toString().should.equal(answers.part1Example.toString());
+    });
+
+    // it('part 1', function () {
+    //     solvePart1(input).toString().should.equal(answers.part1.toString());
+    // });
+});
